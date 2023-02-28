3ed869815d941ec99cd8bea6757c5cffdd07a11a
初始状态0 <chushi0@users.noreply.github.com>
2023-02-28 21:42:43 +0800

    存取款界面交互

--- src/main/java/online/cszt0/mcmod/bank/screen/BankScreen.java
+++ src/main/java/online/cszt0/mcmod/bank/screen/BankScreen.java
@@ -14,7 +14,10 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
@@ -28,6 +31,9 @@ import online.cszt0.mcmod.bank.entity.ClarkVillagerEntity;
 public class BankScreen extends HandledScreen<BankScreen.Handler> {
     public static final ScreenHandlerType<Handler> ScreenType = new ScreenHandlerType<>(Handler::new);
     private static final Identifier TEXTURE = VillageBank.identity("textures/gui/clark.png");
+
+    private static final Text DEPOSIT_TEXT = Text.translatable("ui.village_bank.bank_screen.deposit");
+    private static final Text WITHDRAW_TEXT = Text.translatable("ui.village_bank.bank_screen.withdraw");
 
     public BankScreen(BankScreen.Handler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title);
@@ -68,10 +74,13 @@ public class BankScreen extends HandledScreen<BankScreen.Handler> {
         private UUID entityUuid;
 
         private PlayerInventory inventory;
+        private BankInventory bankInventory;
 
         public Handler(int syncId, PlayerInventory inventory) {
             super(ScreenType, syncId);
+            this.bankInventory = new BankInventory();
             setupInventory(inventory);
+            setupDepositAndWithdraw();
         }
 
         private void setupInventory(PlayerInventory inventory) {
@@ -85,6 +94,12 @@ public class BankScreen extends HandledScreen<BankScreen.Handler> {
                     this.addSlot(new Slot(inventory, j + i * 9 + 9, 108 + j * 18, 84 + i * 18));
                 }
             }
+        }
+
+        private void setupDepositAndWithdraw() {
+            this.addSlot(new DepositSlot(this.bankInventory, BankInventory.INPUT_SLOT, 136, 37));
+            this.addSlot(new DepositSlot(this.bankInventory, BankInventory.INPUT_SLOT, 162, 37));
+            this.addSlot(new WithdrawSlot(this.bankInventory, BankInventory.OUTPUT_SLOT, 220, 37));
         }
 
         @Override
@@ -104,6 +119,108 @@ public class BankScreen extends HandledScreen<BankScreen.Handler> {
         public ItemStack quickMove(PlayerEntity player, int slot) {
             log.info("quickMove");
             return ItemStack.EMPTY;
+        }
+    }
+
+    public static class BankInventory implements Inventory {
+        static final int INPUT_SLOT = 0;
+        static final int OUTPUT_SLOT = 1;
+
+        private int moneyCount = 32;
+
+        @Override
+        public void clear() {
+        }
+
+        @Override
+        public boolean canPlayerUse(PlayerEntity player) {
+            return true;
+        }
+
+        @Override
+        public ItemStack getStack(int slot) {
+            if (slot == OUTPUT_SLOT && moneyCount > 0) {
+                return new ItemStack(Items.EMERALD, Math.min(64, moneyCount));
+            }
+            return ItemStack.EMPTY;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return moneyCount == 0;
+        }
+
+        @Override
+        public void markDirty() {
+        }
+
+        @Override
+        public ItemStack removeStack(int slot) {
+            if (slot == OUTPUT_SLOT && moneyCount > 0) {
+                int removeCount = Math.min(64, moneyCount);
+                moneyCount -= removeCount;
+                return new ItemStack(Items.EMERALD, removeCount);
+            }
+            return ItemStack.EMPTY;
+        }
+
+        @Override
+        public ItemStack removeStack(int slot, int amount) {
+            if (slot == OUTPUT_SLOT && moneyCount > 0) {
+                int removeCount = Math.min(amount, moneyCount);
+                moneyCount -= removeCount;
+                return new ItemStack(Items.EMERALD, removeCount);
+            }
+            return ItemStack.EMPTY;
+        }
+
+        @Override
+        public void setStack(int slot, ItemStack stack) {
+            if (slot == OUTPUT_SLOT) {
+                if (stack.getItem() != Items.EMERALD && !stack.isEmpty()) {
+                    throw new IllegalArgumentException();
+                }
+                int amount = Math.min(64, moneyCount) - stack.getCount();
+                if (amount < 0) {
+                    throw new IllegalArgumentException();
+                }
+                this.moneyCount -= amount;
+                return;
+            }
+            Item item = stack.getItem();
+            if (item == Items.EMERALD) {
+                moneyCount += stack.getCount();
+            } else if (item == Items.EMERALD_BLOCK) {
+                moneyCount += stack.getCount() * 9;
+            } else if (!stack.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        @Override
+        public int size() {
+            return 2;
+        }
+
+    }
+
+    public static class DepositSlot extends Slot {
+        public DepositSlot(Inventory inventory, int index, int x, int y) {
+            super(inventory, index, x, y);
+        }
+
+        public boolean canInsert(ItemStack stack) {
+            return stack.getItem() == Items.EMERALD || stack.getItem() == Items.EMERALD_BLOCK;
+        }
+    }
+
+    public static class WithdrawSlot extends Slot {
+        public WithdrawSlot(Inventory inventory, int index, int x, int y) {
+            super(inventory, index, x, y);
+        }
+
+        public boolean canInsert(ItemStack stack) {
+            return false;
         }
     }
 
